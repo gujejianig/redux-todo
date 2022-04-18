787f70ea837b208a2a73027c1403814651b48192
gujejianig <gujejiani.websmart@gmail.com>
2022-04-18 17:30:23 +0400

    in addTask component deleted list and changed it with reselect library

--- src/components/addTask/index.js
+++ src/components/addTask/index.js
@@ -1,44 +1,45 @@
 import {Button} from "react-bootstrap";
 import React, {useState} from "react";
-import "./searchForm.css"
+import "./searchForm.css";
 import {useDispatch, useSelector} from "react-redux";
 import {ADD_TODO} from "../../redux/actions/Actions";
+import {createSelector} from "reselect";
 
 const SearchForm = () => {
-    const [inputValue, setInputValue] = useState("");
-    const list = useSelector((state) => state)
-    const dispatch = useDispatch()
+	const [inputValue, setInputValue] = useState("");
+	const dispatch = useDispatch();
 
-    const handleKeyDown = (event) => {
-        if (event.key === "Enter") {
-            // submitHandler(event);
-            submitHandler()
-        }
-    };
+	const todos = useSelector(state => state.todos);
+	const todosPerPage = useSelector(state => state.todosPerPage);
 
-    const submitHandler = () => {
-        if (inputValue.trim().length === 0) {
-            alert("input have no value");
-        } else {
-            let nextPage = Math.ceil((list.todos?.length + 1) / list?.todosPerPage )
-            dispatch({type: ADD_TODO, payload: {inputValue: inputValue, nextPage: nextPage}})
-            setInputValue('')
-        }
-    }
+	const handleKeyDown = (event) => {
+		if (event.key === "Enter") {
+			// submitHandler(event);
+			submitHandler();
+		}
+	};
 
-    return (
-        <div className="d-flex w-100 justify-content-sm-center mt-3">
-            <input
-                onKeyUp={handleKeyDown}
-                value={inputValue}
-                className='SearchInput'
-                onChange={(e) => setInputValue(e.target.value)}
-            />
-            <Button size="sm" onClick={submitHandler} variant="primary">
-                Add
-            </Button>
-        </div>
-    );
+	const submitHandler = () => {
+		if (inputValue.trim().length === 0) {
+			alert("input have no value");
+		} else {
+			let nextPage = Math.ceil((todos.length + 1) / todosPerPage);
+			dispatch({type: ADD_TODO, payload: {inputValue: inputValue, nextPage: nextPage}});
+			setInputValue('');
+		}
+	};
+
+	return (<div className="d-flex w-100 justify-content-sm-center mt-3">
+			<input
+				onKeyUp={handleKeyDown}
+				value={inputValue}
+				className="SearchInput"
+				onChange={(e) => setInputValue(e.target.value)}
+			/>
+			<Button size="sm" onClick={submitHandler} variant="primary">
+				Add
+			</Button>
+		</div>);
 };
 
 export default SearchForm;
